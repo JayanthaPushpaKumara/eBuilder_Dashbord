9f711d4aba46cb1846a30ccce54d203787942bed
Jayantha pushpakumara <jpkjayantha@gmail.com>
2017-12-26 17:21:35 +0530

    2017-12-26 5:51pm

--- src/app/frame/line-chart/line-chart.component.ts
+++ src/app/frame/line-chart/line-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartModule } from 'angular2-chartjs';
+//import { ChartModule } from 'angular2-chartjs';
+import { Chart } from 'chart.js';
 
 @Component({
     selector: 'app-line-chart',
@@ -8,72 +9,60 @@ import { ChartModule } from 'angular2-chartjs';
 })
 export class LineChartComponent implements OnInit {
 
-    data: any;
-    options: any;
-
+    
     constructor() {
+        var ctx = document.getElementById("lineChart");
 
-        this.data = {
-            labels: ["January", "February", "March", "April", "May"],
-            datasets: [
-                {
-                    label: "Dataset 1",
-                    fill: false,
-                    lineTension: 0.4,
-                    pointHoverBackgroundColor: "blue",
-                    pointHoverBorderColor: "red",
-                    pointHoverBorderWidth: 2,
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [100, 45, 40, 20, 20],
-                },
-                {
-                    label: "Dataset 2",
-                    borderColor: 'green',
-                    fill: false,
-                    lineTension: .4,
-                    pointHoverBackgroundColor: "blue",
-                    pointHoverBorderColor: "red",
-                    pointHoverBorderWidth: 2,
-                    data: [34, 56, 50, 80, 40],
-                },
-                {
-                    label: "Dataset 3",
-                    borderColor: 'blue',
-                    fill: true,
-                    lineTension: .4,
-                    pointHoverBackgroundColor: "blue",
-                    pointHoverBorderColor: "red",
-                    pointHoverBorderWidth: 2,
-                    data: [20, 35, 60, 40, 65],
-                },
-            ]
-        };
-        this.options= {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+        console.log(ctx);
+
+        new Chart(ctx, {
+
+            type: 'line',
+            data: {
+                labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+                datasets: [{
+                    data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                    label: "Africa",
+                    borderColor: "#3e95cd",
+                    fill: false
+                }, {
+                    data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                    label: "Asia",
+                    borderColor: "#8e5ea2",
+                    fill: false
+                }, {
+                    data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                    label: "Europe",
+                    borderColor: "#3cba9f",
+                    fill: false
+                }, {
+                    data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                    label: "Latin America",
+                    borderColor: "#e8c3b9",
+                    fill: false
+                }, {
+                    data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                    label: "North America",
+                    borderColor: "#c45850",
+                    fill: false
                 }]
             },
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: "Black"
+            options: {
+                title: {
+                    display: true,
+                    text: 'World population per region (in millions)'
                 },
-                position:'bottom'
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'bottom'
+                }
             }
-        }
-    
-
+        });
     }
 
     ngOnInit() {
 
     }
-
-   
-
-    
 }
 
--- src/app/frame/pie-chart/pie-chart.component.ts
+++ src/app/frame/pie-chart/pie-chart.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieChartComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+  options: any;
 
-  ngOnInit() {
+  constructor() {
+
+    this.data = {
+      labels: ["Africa", "Asia", "Europe", "America"],
+      datasets: [{
+        backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
+        data: [2478, 734, 784, 433]
+      }]
+    },
+      this.options = {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        },
+        legend: {
+          display: true,
+          labels: {
+              fontColor: "Black"
+          },
+          position:'bottom'
+      }
+      }
   }
+
+
+ngOnInit() {
+}
 
 }
