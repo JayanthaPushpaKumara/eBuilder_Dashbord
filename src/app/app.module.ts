ea6a2310f0eaa19e2ea386188369207719eb3e39
Jayantha pushpakumara <jpkjayantha@gmail.com>
2018-01-09 10:51:18 +0530

    delete spec.ts // add ng2-chart // change chart1 // add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { ChartModule } from 'angular2-chartjs';
+//import { ChartModule } from 'angular2-chartjs';
+import { AppRoutingModule } from './/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BannerComponent } from './banner/banner.component';
 import { FrameComponent } from './frame/frame.component';
-import { LineChartComponent } from './frame/line-chart/line-chart.component';
-import { PieChartComponent } from './frame/pie-chart/pie-chart.component';
 
+import { LineChartComponent } from './frame/line-chart/line-chart.component';
+import { LineChart2Component } from './frame/line-chart2/line-chart2.component';
+import { PieChartComponent } from './frame/pie-chart/pie-chart.component';
+import { MixedChartComponent } from './frame/mixed-chart/mixed-chart.component';
+import { TestComponent } from './frame/test/test.component';
 import { TableComponent } from './frame/table/table.component';
 import { Pagination}   from './frame/table/app.paginationComponent';
-import { MixedChartComponent } from './frame/mixed-chart/mixed-chart.component';
-import { LineChart2Component } from './frame/line-chart2/line-chart2.component';
+
+import { ChartsModule } from 'ng2-charts';
+
 
 
 @NgModule({
@@ -27,14 +35,19 @@ import { LineChart2Component } from './frame/line-chart2/line-chart2.component';
     TableComponent,
     Pagination,
     MixedChartComponent,
-    LineChart2Component
+    LineChart2Component,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    ChartModule,
-    FormsModule
+    HttpClientModule,
+    ChartsModule,
+    FormsModule,
+    AppRoutingModule
+    
   ],
   providers: [],
+
   bootstrap: [AppComponent,Pagination]
 })
 export class AppModule { }
--- src/app/frame/line-chart/line-chart.component.ts
+++ src/app/frame/line-chart/line-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
     selector: 'app-line-chart',
@@ -8,78 +9,182 @@ import { Chart } from 'chart.js';
 })
 export class LineChartComponent implements OnInit {
 
-
-    constructor() { }
-
+    constructor(private http: HttpClient) { }
 
     ngOnInit() {
 
-        new Chart(document.getElementById("line-chart"), {
-            type: 'line',
-            data: {
-                labels: [1850, 1900, 1950, 2000, 2050],
-                datasets: [{
-                    data: [13, 22, 30, 40, 50],
-                    label: "Africa",
-                    borderColor: "#3e95cd",
-                    fill: false
-                }, {
-                    data: [47, 14, 37, 67, 80],
-                    label: "Asia",
-                    borderColor: "#8e5ea2",
-                    fill: false
-                }, {
-                    data: [40, 47, 50, 60, 100],
-                    label: "Europe",
-                    borderColor: "#3cba9f",
-                    fill: false
-                }, {
-                    data: [74, 67, 75, 84, 90],
-                    label: "Latin America",
-                    borderColor: "#e8c3b9",
-                    fill: false
-                }, {
-                    data: [10, 60, 50, 70, 85],
-                    label: "North America",
-                    borderColor: "#c45850",
-                    fill: false
-                }
-                ]
-            },
-            options: {
-                title: {
-                    display: false,
-                    text: 'World population per region (in millions)'
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            fontColor: "#FFFFFF"
-                        }
-                    }],
-                    xAxes: [{
-                        ticks: {
-                            fontColor: "#FFFFFF"
-                        }
-                    }]
-                },
-                legend: {
-                    labels: {
-                        boxWidth: 30,
-                        fontColor: "#FFFFFF"
-                    },
-                    position: 'bottom',
-                    fullWidth: true
-                },
-                elements: {
-                    line: {
-                        tension: 0, // disables bezier curves
-                    }
-                }
-            }
-        });
+        //this.createChart();
+        this.getData();
 
     }
+
+    public getData(): void {
+        //const that = this;
+        this.http.get('http://www.mocky.io/v2/5a5339103000009b181ebe53').subscribe(
+
+            data => {
+                console.log(data);
+                
+                this.lineChartData = data[0].datasets;
+            },
+
+            (err: HttpErrorResponse) => {
+                if (err.error instanceof Error) {
+                    console.log("Client side Error occured")
+                } else {
+                    console.log("Server side Eror occured")
+                }
+            }
+        );
+    }
+
+
+    public lineChartData: any = [
+        { data: [], label: 'undefined' },
+        { data: [], label: 'undefined' },
+        { data: [], label: 'undefined' },
+        { data: [], label: 'undefined' },
+        { data: [], label: 'undefined' }
+    ];
+
+    //public lineChartData: any = [{ },{ },{ },{ },{ }];
+
+    public lineChartType: string = 'line';
+
+    public lineChartLabels: Array<any> = [1850, 1900, 1950, 2000, 2050];    
+
+    public lineChartOptions: any = {
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    fontColor: "#FFFFFF"
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: "#FFFFFF"
+                }
+            }]
+        },
+        legend: {
+            labels: {
+                boxWidth: 30,
+                fontColor: "#FFFFFF"
+            },
+            position: 'bottom',
+            fullWidth: true
+        },
+
+    };
+
+    public lineChartLegend: boolean = true;
+
+    public lineChartColors: Array<any> = [
+        { // red
+            borderColor: '#FF5E5E',
+            fill: false,
+        },
+        { // green
+            borderColor: '#F5FF5E',
+            fill: false,
+        },
+        { // red
+            borderColor: '#6FFF3A',
+            fill: false,
+        },
+        { // green
+            borderColor: '#3AFFE4',
+            fill: false,
+        },
+        { // blue
+            borderColor: '#3A4BFF',
+            fill: false,
+        }
+      ];
+
+    // events
+    public chartClicked(e: any): void {
+        console.log(e);
+    }
+
+    public chartHovered(e: any): void {
+        console.log(e);
+    }
+
+
+    /*
+        createChart() {
+    
+            new Chart(document.getElementById("line-chart"), {
+                type: 'line',
+                data: {
+                    labels: [1850, 1900, 1950, 2000, 2050],
+                    datasets: [{
+                        data: [13, 22, 30, 40, 50],
+                        label: "Africa",
+                        borderColor: "#3e95cd",
+                        fill: false
+                    }, {
+                        data: [47, 14, 37, 67, 80],
+                        label: "Asia",
+                        borderColor: "#8e5ea2",
+                        fill: false
+                    }, {
+                        data: [40, 47, 50, 60, 100],
+                        label: "Europe",
+                        borderColor: "#3cba9f",
+                        fill: false
+                    }, {
+                        data: [74, 67, 75, 84, 90],
+                        label: "Latin America",
+                        borderColor: "#e8c3b9",
+                        fill: false
+                    }, {
+                        data: [10, 60, 50, 70, 85],
+                        label: "North America",
+                        borderColor: "#c45850",
+                        fill: false
+                    }
+                    ]
+                },
+                options: {
+                    title: {
+                        display: false,
+                        text: 'World population per region (in millions)'
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                fontColor: "#FFFFFF"
+                            }
+                        }],
+                        xAxes: [{
+                            ticks: {
+                                fontColor: "#FFFFFF"
+                            }
+                        }]
+                    },
+                    legend: {
+                        labels: {
+                            boxWidth: 30,
+                            fontColor: "#FFFFFF"
+                        },
+                        position: 'bottom',
+                        fullWidth: true
+                    },
+                    elements: {
+                        line: {
+                            tension: 0, // disables bezier curves
+                        }
+                    }
+                }
+            });
+    
+        }
+    */
+
 }
 
