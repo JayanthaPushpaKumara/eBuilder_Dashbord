9293338872eb2890029425746569093589bb7206
Jayantha pushpakumara <jpkjayantha@gmail.com>
2018-01-17 13:48:13 +0530

    update sidebar message type and regions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InputControls } from './inputControls';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+
+  messageTypes = ['Create Return', 'Event Message', 'Process Return', 'Stock Sync'];
+  regions = ["-All-","APAC" , "brazil" , "east asia" , "europe" , "latin america" , "middle east" , "north america"];
+  marcketUnits =  ["-All-","MU Benelux" ,  "MU Brazil" ,  "MU CHATA" ,  "MU Central & Eastern Europe" , "MU Commonwealth Independent States" ,  "MU France" , "MU Germany" , "MU Greater China" ,  "MU Iberia" ,  "MU India" ,  "MU Latin America" ,  "MU Middle East and Africa" ,  "MU Nordic & BALTIC" ,  "MU North America" ,  "MU SEA and Oceania" ,  "MU SEM" ,  "MU UK & Ireland"]
+
+  model = new InputControls(this.regions[0],this.marcketUnits[0],'2017-10-01','2018-01-01',this.messageTypes[0],'site country');
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  // get diagnostic() { return JSON.stringify(this.model); }
+
+  myFunc(){
+    console.log(JSON.stringify(this.model));
   }
 
 }
