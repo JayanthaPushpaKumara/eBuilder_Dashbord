3223a4239acb7a9a7a5cd1e7f8fb2eb01f54fa39
Jayantha pushpakumara <jpkjayantha@gmail.com>
2018-01-29 13:20:41 +0530

    change wip and msg failure pr -ue using ng2 chart

--- src/app/frame/message-failure-pr-ue/message-failure-pr-ue.component.ts
+++ src/app/frame/message-failure-pr-ue/message-failure-pr-ue.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-message-failure-pr-ue',
@@ -8,70 +8,116 @@ import { Chart } from 'chart.js';
 })
 export class MessageFailurePrUeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-
-    new Chart(document.getElementById("line-chart2"), {
-      type: 'line',
-      data: {
-        labels: ["2018-01-16","2018-01-18","2018-01-20","2018-01-22"],
-        datasets: [{
-          data: [57,57,58,122],
-          label: "PR FAILURE",
-          borderColor: "#3e95cd",
-          fill: false
-        }, {
-          data: [14,100],
-          label: "PR TOTAL",
-          borderColor: "#8e5ea2",
-          fill: false
-        }, {
-          data: [3,3,3,3],
-          label: "UE FAILURE",
-          borderColor: "orange",
-          fill: false
-        }, {
-          data: [5,3],
-          label: "UE TOTAL",
-          borderColor: "#3cba9f",
-          fill: false
-        }]
-      },
-      options: {
-        title: {
-          display: false,
-          text: 'World population per region (in millions)'
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              fontColor: "#FFFFFF"
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              fontColor: "#FFFFFF"
-            }
-          }]
-        },
-        legend: {
-          labels: {
-            boxWidth: 30,
-            fontColor: "#FFFFFF"
-          },
-          position: 'bottom',
-          fullWidth: true
-        },
-        elements: {
-          line: {
-            tension: 0, // disables bezier curves
-          }
-        }
-      }
-    });
-
+    this.getData();
   }
 
+  
+
+  public getData(): void {
+    this.http.get('http://www.mocky.io/v2/5a6ec8bb30000008106fe30e').subscribe(
+      data => {
+        console.log(data);
+        this.lineChartData = data;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+            console.log("Client side Error occured")
+        } else {
+            console.log("Server side Eror occured")
+        }
+    }
+    );
+  }
+
+  public lineChartData: any = [{ },{ },{ },{ }];
+
+
+  public lineChartType: string = 'line';
+
+  public lineChartLabels: Array<any> = ["2018-01-16","2018-01-18","2018-01-20","2018-01-22"];
+  public lineChartOptions: any = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          fontColor: "#FFFFFF"
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: "#FFFFFF"
+        }
+      }]
+    },
+    legend: {
+      labels: {
+        boxWidth: 30,
+        fontColor: "#FFFFFF"
+      },
+      position: 'bottom',
+      fullWidth: true
+    },
+    elements: {
+      line: {
+        tension: 0, // disables bezier curves
+      }
+    }
+
+  };
+
+  public lineChartColors: Array<any> = [
+    { // red
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // blue
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'blue',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // blue
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'green',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // blue
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'Maroon',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  public lineChartLegend: boolean = true;
+
+
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+
+
 }
+
--- src/app/frame/wip/wip.component.ts
+++ src/app/frame/wip/wip.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-wip',
@@ -8,73 +8,105 @@ import { Chart } from 'chart.js';
 })
 export class WipComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit() {
-
-    new Chart(document.getElementById("mixed-chart"), {
-      type: 'bar',
-      data: {
-        labels: ["1850", "1900", "1950", "1999", "2050"],
-        datasets: [{
-          label: "Europe",
-          type: "line",
-          borderColor: "#8e5ea2",
-          data: [100, 150, 300, 600, 700],
-          fill: false
-        }, {
-          label: "Africa",
-          type: "line",
-          borderColor: "#3e95cd",
-          data: [250, 400, 500, 675, 800],
-          fill: false
-        }, {
-          label: "Europe",
-          type: "bar",
-          backgroundColor: "#706EFF",
-          data: [100, 150, 300, 600, 700]
-        }, {
-          label: "Africa",
-          type: "bar",
-          backgroundColor: "#4DFFEF",
-          backgroundColorHover: "#3e95cd",
-          data: [250, 400, 500, 675, 800]
-        }
-        ]
-      },
-      options: {
-        title: {
-          display: false,
-          text: 'Population growth (millions): Europe & Africa'
-        },
-
-        scales: {
-          yAxes: [{
-            ticks: {
-              fontColor: "#FFFFFF"
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              fontColor: "#FFFFFF"
-            }
-          }]
-        },
-        legend: {
-          labels: {
-            boxWidth: 30,
-            fontColor: "#FFFFFF"
-          },
-          position: 'bottom',
-          fullWidth: true
-        },
-        elements: {
-          line: {
-            tension: 0.3, // disables bezier curves
-          }
-        }
-      }
-    });
+    this.getData();
   }
+
+  public getData(): void {
+    this.http.get('http://www.mocky.io/v2/5a6ecc5230000003116fe31b').subscribe(
+      data => {
+        console.log(data);
+        this.lineChartData = data;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+            console.log("Client side Error occured")
+        } else {
+            console.log("Server side Eror occured")
+        }
+    }
+    );
+  }
+
+  public lineChartData: any = [{ },{ },{ }];
+
+
+  public lineChartType: string = 'line';
+
+  public lineChartLabels: Array<any> = ["2018-01-22","2018-01-23","2018-01-24","2018-01-25","2018-01-26"];
+  public lineChartOptions: any = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false,
+          fontColor: "#FFFFFF"
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: "#FFFFFF"
+        }
+      }]
+    },
+    legend: {
+      labels: {
+        boxWidth: 30,
+        fontColor: "#FFFFFF"
+      },
+      position: 'bottom',
+      fullWidth: true
+    },
+    elements: {
+      line: {
+        tension: 0, // disables bezier curves
+      }
+    }
+
+  };
+
+  public lineChartColors: Array<any> = [
+    { // red
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'green',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // blue
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'blue',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // blue
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'yellow',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  public lineChartLegend: boolean = true;
+
+
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+
 
 }
